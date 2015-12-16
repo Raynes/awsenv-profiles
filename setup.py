05ee83ce5a931eeaeeabb271eb9592bcfa950c80
Anthony Grimes <i@raynes.me>
2015-12-16 13:00:49 -0800

    Rename package in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="awsenv",
+    name="awsenv-profiles",
     version="0.0.1",
     author="Anthony Grimes",
     author_email="awsenv@raynes.me",
     include_package_data=True,
-    url="https://github.com/Raynes/awsenv",
+    url="https://github.com/Raynes/awsenv-profiles",
     install_requires=[
         "botocore==1.3.12"
     ],
